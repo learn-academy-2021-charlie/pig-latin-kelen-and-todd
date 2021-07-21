07c0b05cc67dbd3d2ab712bb40351264edac17cc
ToddComte <comtetodd@gmail.com>
2021-07-21 16:36:18 -0600

    adds comments describing program

--- src/App.js
+++ src/App.js
@@ -7,93 +7,75 @@ class App extends Component{
     super(props)
     // the state object holds information that can be displayed to the user and updated throughout the program
     this.state = {
-      // "phrase" is the text entered by the user - right now there are test words hard coded to make the process of testing your code faster and easier
-      // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "alpha through yummy squeal queen fry",
+      // "phrase" is the text entered by the user 
+      
+      phrase: "",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
-      phraseTranslated: "This is where your translated sentence will appear."
+      phraseTranslated: ""
     }
   }
   translateWord = (string) => {
-    let solution = [];
-    let array = string.split('');
+    let solution = []; // holds current word in pig latin
+    let array = string.split(''); // holds current word as given 
     const vowels = ['a','e','i','o','u']
-    let punctuations = [".", ",", ":", "!", "?"]
+  
 
-    if(array.length === 1){
+    if(array.length === 1){ // edge case for single letter words
       solution = array.concat(['w', 'a', 'y'])
     }
-    if(array[0] && !!array[0].match(/(\W+)/)){
+    if(array[0] && !!array[0].match(/(\W+)/)){ // keeps the current word as-is if it is only punctuation 
       solution = [string]
     }
-    if(vowels.includes(array[0])){
+    if(vowels.includes(array[0])){ // if first leeter is a vowel add way to the end
       solution = array.concat(['w', 'a', 'y'])
     }
     else{
-        for(let i = 0; i < array.length; i++){
-          if(vowels.includes(array[i].toLowerCase()) && array[i-1].toLowerCase() != 'q'){
+        for(let i = 0; i < array.length; i++){ 
+          if(vowels.includes(array[i].toLowerCase()) && array[i-1].toLowerCase() !== 'q'){ // this handles the words that don't have "y" as a vowel
+            solution = array.slice(i).concat(array.slice(0, i)).concat(['a', 'y']) // ingnores "u" when preceded by "q"
+            break
+          } else if(array[i].toLowerCase() === 'y' && i !== 0){ // handles a "y" as a vowel
             solution = array.slice(i).concat(array.slice(0, i)).concat(['a', 'y'])
             break
-          } else if(array[i].toLowerCase() === 'y' && i != 0){
-            solution = array.slice(i).concat(array.slice(0, i)).concat(['a', 'y'])
-            break
-          }
+          } 
         }
     }
-    return solution.join('');
+    return solution.join(''); // returns our solution as a string
   }
 
   // The "myPigLatinCodeHere" function is where you will put your logic to convert the sentence entered by the user to Pig Latin
 
   myPigLatinCodeHere = () => {
-    let punctuations = [".", ",", ":", "!", "?"]
     // the variable "userInput" will contain the text input from the user modified into an array of words
     // no need to change this variable
     let userInput = this.state.phrase.split(/(\W+)/)
-    //console.log("userInput:", userInput)
-    let answerArray = []
+  var answerArray = []
     // now that we have an array of words, we can map over the array and access each word
-    let translatedWordsArray = userInput.map(currentWord => {
+     let translatedWordsArray = userInput.map(currentWord => {
       // ACTION ITEM: use "currentWord" as a starting point for your code
-      //console.log("currentWord:", currentWord)
       answerArray.push(this.translateWord(currentWord))
-
-
-      // your code here!
-
-      // Remember: console.log is your friend :)
-
-
-      // ACTION ITEM: change the value of currentWord to the name of whatever variable you made containing your Pig Latin'd word
       return currentWord
     })
     
 
     for(let i = 0; i < answerArray.length; i++){
-      if(!!answerArray[i].match(/(\W+)/)){
-        answerArray[i-1] = answerArray[i-1].concat(answerArray[i])
-        answerArray.splice(i, 1)
+      if(!!answerArray[i].match(/(\W+)/)){ // check fo punctuation values
+        answerArray[i-1] = answerArray[i-1].concat(answerArray[i]) // rejoines punctuation with its word
+        answerArray.splice(i, 1) // removes redundant punctuation
       }
     }
-    console.log(answerArray)
 
     this.setState({
-      phraseTranslated: answerArray.join(' ')
+      phraseTranslated: answerArray.join(' ') // showes our translated phrase
     })
-
-    // joining the array back to a string of translated words
-    // no need to change this variable
-
     // the setState method will take your information from "translatedWords" and update the state object that is displayed to the user
-    // no need to change this method
   }
-
   restartGame = () => {
     // this method restarts the game by setting the original state
     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
     this.setState({
-      phrase: "alpha through yummy squeal queen fry",
-      phraseTranslated: "This is where your translated sentence will appear."
+      phrase: "",
+      phraseTranslated: ""
     })
   }
 
@@ -134,7 +116,7 @@ class App extends Component{
           <button onClick={this.restartGame}>Clear</button>
         </div>
         <p>{this.state.phraseTranslated}</p>
-        <footer>Coded by: Kelen, Todd, Chauncy</footer>
+        <footer>Coded by: Team: we are the bomb! Kelen, Todd, Chauncy</footer>
       </>
     )
   }
